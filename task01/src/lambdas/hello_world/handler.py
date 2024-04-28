32cb499e155fc415bb9ef355586839a83722532a
Yuriy Shevtsov <external.iurii.shevtsov@bosch.com>
2024-04-28 17:39:21 +0200

    first hello world

---
+++ task01/src/lambdas/hello_world/handler.py
@@ -0,0 +1,28 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        _LOG.info(f"Received event: {event} \t {repr(event)}")
+        _LOG.info(f"Received context: {context} \t {repr(context)}")
+        return {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        }
+
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
