21c83d32c3cc3f278e170ee155c05177ec87151d
Yuriy Shevtsov <external.iurii.shevtsov@bosch.com>
2024-07-04 18:46:57 +0200

    add task02

---
+++ task02/src/lambdas/hello_world/handler.py
@@ -0,0 +1,32 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        method = event.get('httpMethod') or 'NONE'
+        path = event.get('path') or 'NONE'
+        _LOG.info(f"Received event: {event} \t {repr(event)}")
+        _LOG.info(f"Received context: {context} \t {repr(context)}")
+        _LOG.info(f"HTTP Method: {method}")
+        _LOG.info(f"Path: {path}")
+        return {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        } if '/hello' == path.lower() and 'get' == method.lower() else {
+            "statusCode": 400,
+            "message": f"Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+        }
+
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
