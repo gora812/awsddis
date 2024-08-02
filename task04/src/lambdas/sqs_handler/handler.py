fb171e874b68c3dfbb4dc10d343fdf115ee73193
Yuriy Shevtsov <external.iurii.shevtsov@bosch.com>
2024-08-02 16:16:32 +0200

    added task04

---
+++ task04/src/lambdas/sqs_handler/handler.py
@@ -0,0 +1,21 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('SqsHandler-handler')
+
+
+class SqsHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        _LOG.log(event)
+        return 200
+    
+
+HANDLER = SqsHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
